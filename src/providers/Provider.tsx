4dd7736240de2d1db3ede0588ac1ab31c36eccf2
antonisoaho <isoahoanton@gmail.com>
2024-10-28 15:08:07 +0100

    Setup providertheme

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -1,14 +1,30 @@
-import { ChakraProvider, createSystem, defineConfig } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  createSystem,
+  defineConfig,
+  defaultConfig,
+} from "@chakra-ui/react";
 
 const config = defineConfig({
   theme: {
     tokens: {
-      colors: {},
+      colors: {
+        light: { value: "#d6c9ff" },
+        primary: { value: "#8e6eff" },
+        dark: { value: "#4b1fb3" },
+        error: { value: "#ff1035" },
+        success: { value: "#28a745" },
+        customText: { value: "#333333" },
+      },
+      fonts: {
+        heading: { value: "'Poppins', sans-serif" },
+        body: { value: "'Roboto', sans-serif" },
+      },
     },
   },
 });
 
-const system = createSystem(config);
+const system = createSystem(defaultConfig, config);
 
 interface ProviderProps {
   children: React.ReactNode;
