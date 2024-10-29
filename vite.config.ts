f13d17ecb7a3fcf6cab30e08d9b6be33e93f1c0d
antonisoaho <isoahoanton@gmail.com>
2024-10-29 14:33:50 +0100

    Jadu

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const branch = process.env.VITE_BRANCH;
-
 export default defineConfig({
-  base: branch === "dev" ? "/Meetups-FE/" : "/",
-  plugins: [react()],
+  plugins: [react()]
 });
