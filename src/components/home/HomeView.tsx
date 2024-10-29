f844f181230cb59085ce198bd9dd7f00f0381f3a
cozyhorse <100480213+cozyhorse@users.noreply.github.com>
2024-10-29 18:24:40 +0100

    searchbar alpha

--- src/components/home/HomeView.tsx
+++ src/components/home/HomeView.tsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 // import { Button } from '../ui/Button';
 import Register from "../auth/Register";
 import { Button } from "@mui/material";
+import Searchbar from "../searchbar/Searchbar";
+import { Search } from "@mui/icons-material";
 
 const HomeView = () => {
   return (
     <>
       <p>HomeView</p>
+      <Searchbar />
       <Register />
     </>
   );
