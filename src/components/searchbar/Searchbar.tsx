b89b45b763c8a63be5dd16012d846c3fdfdd86c3
cozyhorse <100480213+cozyhorse@users.noreply.github.com>
2024-10-31 13:15:56 +0100

    added search by date

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -1,28 +1,40 @@
 import * as React from "react";
-import { TextField, Box } from "@mui/material";
+import { TextField, Grid2 } from "@mui/material";
+import { DateField, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Dayjs } from "dayjs";
 
 interface SearchProps {
   inputText: string;
   setInputText: React.Dispatch<React.SetStateAction<string>>;
+  setDate: React.Dispatch<React.SetStateAction<Dayjs | null>>;
+  date: Dayjs | null;
 }
 
-
-const Searchbar = ({inputText, setInputText}: SearchProps) => {
+const Searchbar = ({ inputText, setInputText, date, setDate }: SearchProps) => {
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const lowerCase = e.target.value.toLowerCase();
     setInputText(lowerCase);
   };
 
+  const dateHandler = (newDate: Dayjs | null) => {
+    setDate(newDate);
+  }
+
   return (
     <>
-      <Box>
+      <Grid2 container spacing={2}>
         <TextField
           value={inputText}
           onChange={inputHandler}
           placeholder="Meetups or Location"
           label="Search"
         />
-      </Box>
+        
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateField label="Filter on date" value={date} onChange={dateHandler} />
+    </LocalizationProvider>
+      </Grid2>
     </>
   );
 };
